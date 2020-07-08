8505da32f2f4ca95b4684378b45466c40d4587df
Sage LaTorra <Sagelt@gmail.com>
2020-07-08 08:11:00 -0700

    Alphabetize playbook order.
    Fixes #3. Thanks Adam!

--- tools/get_files_to_compile.sh
+++ tools/get_files_to_compile.sh
@@ -4,9 +4,8 @@ rootdir=`git rev-parse --show-toplevel`
 
 echo \
   $rootdir/text/introduction.md \
+  $rootdir/text/cleric/cleric.md \
+  $rootdir/text/cleric/divine-patronage.md \
   $rootdir/text/wizard/wizard.md \
   $rootdir/text/wizard/spellbook.md \
   $rootdir/text/wizard/mystic_patron.md \
-  $rootdir/text/cleric/cleric.md \
-  $rootdir/text/cleric/divine-patronage.md \
-  
